03d79a768aef795ec56bffa77ae626cf24f44ede
Danilo Sacramento <89158678+DaniloSacramento@users.noreply.github.com>
2022-09-21 14:58:02 -0300

    Funcao no botao

--- lib/main.dart
+++ lib/main.dart
@@ -4,16 +4,45 @@ main() => runApp(PerguntaApp());  // INSTANCIEI O PERGUNTA APP
 
 
 class PerguntaApp extends StatelessWidget{ 
+  void responder() { 
+    
+      print("Pergunta respondida");
+    
+  }
+  
+ 
 
   @override
+   
   Widget build (BuildContext context){  // CRIAMOS UM METODO 
+    
+    final List <String> perguntas = [ "Qual é sua cor favorita?",  "Qual é seu time?", "Qual seu animal favorito?",
+  ]; 
+
     return  MaterialApp(
-      home:  Scaffold(
+      home:  Scaffold (
         appBar: AppBar(
           title: Text("Escolha seu time"),     // O TITLE É UM PARAMETRO NOMEADO QUE RECEBE UM WIDGET
         ),
-         body: Text("Olá Flutter"),
-      )
-    );
+         body:  Column(
+           children:  <Widget>  [
+             Text(perguntas.elementAt(1)),
+              ElevatedButton(
+              child:  Text("Resposta 1") ,  // UM BOTAO QUE RECEBE UM FILHO = CHILD
+              onPressed: responder,  // NÃO ESTA PASSANDO NENHUMA FUNÇÃO PARA SER EXECUTADA NO BOTAO  
+             ),
+              ElevatedButton(
+              child: Text("Resposta 2") ,  
+              onPressed: responder,  
+             ),
+               ElevatedButton(
+              child: Text("Resposta 3") ,  
+              onPressed: () => print("Resposta 3!"),  
+             )
+           ],
+         ),
+         ),
+      );
+    
   }
 }
