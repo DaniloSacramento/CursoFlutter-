7a21939d3a76950e5020b0b7f15f6a36360153ac
Danilo Sacramento <89158678+DaniloSacramento@users.noreply.github.com>
2022-09-21 13:54:14 -0300

    Aprendendo o flutter

--- lib/main.dart
+++ lib/main.dart
@@ -7,8 +7,13 @@ class PerguntaApp extends StatelessWidget{
 
   @override
   Widget build (BuildContext context){  // CRIAMOS UM METODO 
-    return const MaterialApp(
-      home:  Text("Olá Flutter!!!"),
+    return  MaterialApp(
+      home:  Scaffold(
+        appBar: AppBar(
+          title: Text("Escolha seu time"),     // O TITLE É UM PARAMETRO NOMEADO QUE RECEBE UM WIDGET
+        ),
+         body: Text("Olá Flutter"),
+      )
     );
   }
 }
