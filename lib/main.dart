dc5b9c86ea5b3a6f0744693f172fd91ba5853849
Danilo Sacramento <89158678+DaniloSacramento@users.noreply.github.com>
2022-09-22 15:50:10 -0300

    Resolvendo o problema do erro da tela

--- lib/main.dart
+++ lib/main.dart
@@ -2,68 +2,73 @@ import 'package:flutter/material.dart';
 import './Questao.dart';
 import './Respostas.dart';
 
-main() => runApp(PerguntaApp());  // INSTANCIEI O PERGUNTA APP
+main() => runApp(PerguntaApp()); // INSTANCIEI O PERGUNTA APP
 
 class _PerguntaAppState extends State<PerguntaApp> {
+  var _PerguntaSelecionada = 0;
 
-   var _PerguntaSelecionada = 0;
+  final List<Map<String, Object>> _perguntas = const [
+    {
+      "texto": "Qual é seu time?",
+      "respostas": ["Nautico", "Sport", "Santa Cruz", "Salgueiro"]
+    },
+    {
+      "texto": "Qual seu jogador favorito?",
+      "respostas": ["Jean Carlos", "Kieza", "Diego Souza", "Denis Marques"],
+    },
+    {
+      "texto": "Qual seu estadio de futebol favorito?",
+      "respostas": ["Aflitos", "Arruda", "Ilha do Retiro", "Arena Pernambuco"]
+    }
+  ];
 
-   
+  void _responder() {
+    // Metodo responder
+    if (temPerguntaSelecionada) {
+      setState(() {
+        _PerguntaSelecionada++; // DENTRO DO SETSTATE VOCE PASSA AQUILO QUE ESTÁ SENDO MODIFICADO
+      });
+    }
+  }
 
-  
-  void _responder() {     
-     setState(() {
-        _PerguntaSelecionada++;   // DENTRO DO SET STATE VOCE PASSA AQUILO QUE ESTÁ SENDO MODIFICADO
-     });
-     
-
-       }
+  bool get temPerguntaSelecionada {
+    return _PerguntaSelecionada < _perguntas.length;
+  }
 
   @override
-   
-  Widget build (BuildContext context){  // CRIAMOS UM METODO 
-    
-    final List<Map<String, Object>> perguntas = [ 
-      {
-        "texto" : "Qual é seu time?",
-        "respostas": ["Nautico" ,  "Sport" , "Santa Cruz", "Salgueiro"]
-      },
-      {
-        "texto" : "Qual seu jogador favorito?",
-        "respostas": ["Jean Carlos", "Kieza", "Diego Souza", "Denis Marques"], 
-      },
-      {
-        "texto": "Qual seu estadio de futebol favorito?",
-        "respostas": ["Aflitos", "Arruda", "Ilha do Retiro", "Arena Pernambuco"]
-      }
-  ]; 
-  List<Widget> resposta = [];
+  Widget build(BuildContext context) {
+    // CRIAMOS UM METODO
 
-  for(var textoResp in perguntas[_PerguntaSelecionada].cast()["respostas"]){
-     resposta.add(Resposta(textoResp, _responder));
-  }
-    return  MaterialApp(
-      home:  Scaffold (
+    List<String> respostas = temPerguntaSelecionada
+        ? _perguntas[_PerguntaSelecionada].cast()["respostas"]
+        : [];
+
+    return MaterialApp(
+      home: Scaffold(
         appBar: AppBar(
-          title: Text("Escolha seu time"),     // O TITLE É UM PARAMETRO NOMEADO QUE RECEBE UM WIDGET
+          title: Text( "Escolha seu time"), // O TITLE É UM PARAMETRO NOMEADO QUE RECEBE UM WIDGET
         ),
-         body:  Column(
-           children:  <Widget>  [
-             Questao(perguntas[_PerguntaSelecionada]["texto"].toString()),
-            ...resposta,
-            
-               
-           ],
-         ),
-         ),
-      );
-
+        body: temPerguntaSelecionada
+            ? Column(
+                children: <Widget>[
+                  Questao(_perguntas[_PerguntaSelecionada]["texto"].toString()),
+                  ...respostas.map((t) => Resposta(t, _responder)).toList(),
+                ],
+              )
+            : const Center(
+                child: Text(
+                  "Obrigador por responder!",
+                  style: TextStyle(fontSize: 30),
+                ),
+              ),
+      ),
+    );
   }
 }
-class PerguntaApp extends StatefulWidget{ 
-  
- @override
+
+class PerguntaApp extends StatefulWidget {
+  @override
   _PerguntaAppState createState() {
-     return _PerguntaAppState();
+    return _PerguntaAppState();
   }
 }
